b9afa7fc29c25677893c370bca3d05f517e37001
Dex Fitch <fitch.dexter@gmail.com>
2015-02-09 12:39:35 -0800

    Refactor JS functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,33 @@
-var queenAttack = function(queenPosition, attackPosition) {
-  var qpx = queenPosition[0];
-  var qpy = queenPosition[1];
-  var apx = attackPosition[0];
-  var apy = attackPosition[1];
-
-  if ((qpx < 1) || (qpy < 1) || (apx < 1) || (apy < 1) || (qpx > 8) || (qpy > 8) || (apx > 8) || (apy > 8)) {
+var lessThanZero = function (coordinate) {
+  if (coordinate < 1) {
+    return true;
+  } else {
     return false;
-  } else if ((qpx === apx) || (qpy === apy) || ((qpx - apx) + (qpy - apy) === 0) || ((qpx - apx) - (qpy - apy) === 0)) {
+  }
+};
+
+var greaterThanEight = function (coordinate) {
+  if (coordinate > 8) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var queenAttack = function(queenPosition, attackPosition) {
+  var queenPositionX = queenPosition[0];
+  var queenPositionY = queenPosition[1];
+  var attackPostionX = attackPosition[0];
+  var attackPostionY = attackPosition[1];
+  var allCoordinates = [queenPositionX, queenPositionY, attackPostionX, attackPostionY]
+
+  if ((allCoordinates.some(lessThanZero)) || (allCoordinates.some(greaterThanEight))) {
+    return false;
+  } else if (queenPositionX === attackPostionX) {
+    return true;
+  } else if (queenPositionY === attackPostionY) {
+    return true;
+  } else if ((Math.abs(queenPositionX - attackPostionX)) - (Math.abs(queenPositionY - attackPostionY)) === 0) {
     return true;
   } else {
     return false;
