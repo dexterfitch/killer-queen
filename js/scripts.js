53a5cf79b42dd388deabd27feadb8daddd26b29f
Dex Fitch <fitch.dexter@gmail.com>
2015-02-09 11:57:25 -0800

    Add complete working functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 var queenAttack = function(queenPosition, attackPosition) {
-  if(queenPosition[0] === attackPosition[0]){
+  var qpx = queenPosition[0];
+  var qpy = queenPosition[1];
+  var apx = attackPosition[0];
+  var apy = attackPosition[1];
+
+  if ((qpx < 1) || (qpy < 1) || (apx < 1) || (apy < 1) || (qpx > 8) || (qpy > 8) || (apx > 8) || (apy > 8)) {
+    return false;
+  } else if ((qpx === apx) || (qpy === apy) || ((qpx - apx) + (qpy - apy) === 0) || ((qpx - apx) - (qpy - apy) === 0)) {
     return true;
   } else {
-  return false;
+    return false;
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,39 @@
 describe('queenAttack', function() {
   it("is false if the coordinates are not horizontally vertically or diagonally in line with each other", function() {
-    expect(queenAttack([1, 1], [2,4])).to.equal(false);
+    expect(queenAttack([1, 1], [2, 4])).to.equal(false);
   });
 
   it("is true if the attack piece is horizontal from the queen piece", function() {
-    expect(queenAttack([1, 1], [1,4])).to.equal(true);
+    expect(queenAttack([1, 1], [1, 4])).to.equal(true);
   });
+
+  it("is true if the attack piece is vertical from the queen piece", function() {
+    expect(queenAttack([3, 5], [7, 5])).to.equal(true);
+  });
+
+  it("is true if the attack piece is lower-left-diagional from the queen piece", function() {
+    expect(queenAttack([1, 7], [4, 4])).to.equal(true);
+  });
+
+  it("is true if the attack piece is upper-right-diagional from the queen piece", function() {
+    expect(queenAttack([6, 1], [2, 5])).to.equal(true);
+  });
+
+  it("is true if the attack piece is upper-left-diagional from the queen piece", function() {
+    expect(queenAttack([6, 3], [5, 2])).to.equal(true);
+  });
+
+  it("is true if the attack piece is lower-right-diagional from the queen piece", function() {
+    expect(queenAttack([5, 4], [7, 6])).to.equal(true);
+  });
+
+  it("is false if any specified coordinate is less than one", function() {
+    expect(queenAttack([0, 7], [2, 5])).to.equal(false);
+  });
+
+  it("is false if any specified coordinate is greater than 8", function() {
+    expect(queenAttack([3, 4], [8, 9])).to.equal(false);
+  });
+
 
 });
